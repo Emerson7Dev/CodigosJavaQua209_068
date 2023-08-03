c1162fd60139494af7f6bc70dd6a573cd3614455
Emerson7Dev <emerson.profisional7@gmail.com>
2023-08-03 17:21:03 -0300

    arquivos adicionados

---
+++ Olamundo/src/principal/OlaMundo.java
@@ -0,0 +1,22 @@
+package principal;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //imprime uma linha e ao final salta uma nova linha 
+	    //print = imprimir; = new line;
+		System.out.println("Hello world, Olá Mundo!");
+		
+		//comentario de uma linha
+	    /*
+	     * esse é
+	     *um comentario
+	     *de multiplas linhas
+	     */
+		
+		
+	}
+	
+
+}
